82142434bb18732bccf4b03f7a482454bc0896d7
Vladimir Matos <vladimir.matos@outlook.com>
2021-03-30 19:42:03 -0300

    feat (initial): initial version
    initial commit version of project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# HangFire-Services
-A implementation of services in .net core with hangfire dashboard
+# HangFire Services
+*A implementation of services in .net core with hangfire dashboard*
+
+
+## Using
+- .Net Core 3.1
+- Web API
+
+
+### How to use
+
+> - Run API as Kestrel  
+> - dotnet run  
+> - Application open port 5000  
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HangFire_Services
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			
+		}
+		
+		public void Configure(IApplicationBuilder application, IWebHostEnvironment environment)
+		{
+			if (environment.IsDevelopment())
+			{
+				application.UseDeveloperExceptionPage();
+			}
+
+			application.UseRouting();
+			application.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Hello World!");
+				});
+			});
+		}
+	}
+}
