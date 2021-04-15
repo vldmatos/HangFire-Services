2e72ae9b04adeba091bc1f04c23c4f30f5404b74
Vladimir Matos <vladimir.matos@outlook.com>
2021-04-15 12:55:21 -0300

    feat (schedule): add execution schedule
    add method to execute with schedule

--- Process.cs
+++ Process.cs
@@ -5,12 +5,22 @@ namespace HangFire.Services
 {
 	public class Process
 	{
-		private const string cron = "*/5 * * * * *";
+		private const string cron = "*/5 * * * * *";		
 
 		public static void Start()
 		{
-			RecurringJob.AddOrUpdate(() => Console.Write($"Alpha Process: {DateTime.Now}"), cron);
-			RecurringJob.AddOrUpdate(() => Console.Write($"Beta Process: {DateTime.Now}"), cron);
+			RecurringJob.AddOrUpdate(() => ExecuteWithCron(), cron);
+			BackgroundJob.Schedule(() => ExecuteWithSchedule(), DateTimeOffset.FromUnixTimeSeconds(10));
 		}
+		
+		public static void ExecuteWithCron()
+		{
+			Console.WriteLine("$Process with Cron { DateTime.Now}");
+		}
+
+		public static void ExecuteWithSchedule()
+		{
+			Console.WriteLine("$Process with Schedule { DateTime.Now}");
+		}		
 	}
 }
